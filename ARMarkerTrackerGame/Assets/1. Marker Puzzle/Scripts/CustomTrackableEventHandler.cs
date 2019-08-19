e2288000ae4935c6424ba988f40e925ef758ec7e
Amshu <amshumaanrakshith@gmail.com>
2019-08-19 21:31:23 +1000

    Added a custom event handler script

---
+++ ARMarkerTrackerGame/Assets/1. Marker Puzzle/Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+    bool isScanned = false;
+    [SerializeField] PlacementScript placeScript = null;
+    [SerializeField] GameObject objectToActivate = null;
+
+    #region OVERRIDED_METHODS
+
+    override protected void Start()
+    {
+        base.Start();
+
+        objectToActivate.SetActive(false);
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        //base.OnTrackingFound();
+
+        objectToActivate.SetActive(true);
+
+        if (!isScanned)
+        {
+            isScanned = true;
+            placeScript.EnableButton();
+        }
+    }
+
+    protected override void OnTrackingLost()
+    {
+        //base.OnTrackingLost();
+    }
+
+    #endregion
+}
